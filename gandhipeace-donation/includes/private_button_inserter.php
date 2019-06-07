706b3e19fcb5363b5a5bfab61706d33fd0577536
Anukul Sharma <anukul.sharma@my.jcu.edu.au>
2019-06-07 12:04:30 +1000

    Update private_button_inserter.php
    This is the plugin page to insert buttons

--- gandhipeace-donation/includes/private_button_inserter.php
+++ gandhipeace-donation/includes/private_button_inserter.php
@@ -2,54 +2,55 @@
 /*
 Plugin Name: Gandhipeace Donation
 Plugin URI: http://anukuls.sgedu.site/plugin
-Description: Gandhipeace Donation is the donation plugin that gives user everything that need to start accepting donations today. Designed to integrate seamlessly with WordPress, Gandhipeacedonation allows the organization to create powerful fundraising platforms on their own website.Easy and simple setup and insertion of PayPal donate buttons with a shortcode or through a sidebar Widget. Donation purpose can be set for each button. A few other customization options are available as well.
+Description: Gandhipeace Donation is the donation plugin that gives user everything that need to start accepting donations for GSML.
 Author: Anukul Sharma
 Author URI: http://anukuls.sgedu.site/
 Version: 1.0.0
 License: GPLv2 or later
 Text Domain: gandhipeace-donations
-*/add_action('init', 'wpedon_button_media_buttons_init');
+*/
+add_action('init', 'gsml_button_media_buttons_init');
 
-function wpedon_button_media_buttons_init() {
+function gsml_button_media_buttons_init() {
 	global $pagenow, $typenow;
 
 	// add media button for editor page
 	if ( in_array( $pagenow, array( 'post.php', 'page.php', 'post-new.php', 'post-edit.php' ) ) && $typenow != 'download' ) {
 		
-		add_action('admin_footer', 'wpedon_button_add_inline_popup_content');
-		add_action('media_buttons', 'wpedon_button_add_my_media_button', 20);
+		add_action('admin_footer', 'gsml_button_add_inline_popup_content');
+		add_action('media_buttons', 'gsml_button_add_my_media_button', 20);
 		
 		// button
-		function wpedon_button_add_my_media_button() {
-			echo '<a href="#TB_inline?width=600&height=500&inlineId=wpedon_popup_container" title="Insert a PayPal Donation Button" id="insert-my-media" class="button thickbox">PayPal Donation Button</a>';
+		function gsml_button_add_my_media_button() {
+			echo '<a href="#TB_inline?width=600&height=500&inlineId=gsml_popup_container" title="Insert a Gamdhipeace PayPal Donation Button" id="insert-my-media" class="button thickbox">Gandhipeace PayPal Donation Button</a>';
 		}
 		
 		// popup
-		function wpedon_button_add_inline_popup_content() {
+		function gsml_button_add_inline_popup_content() {
 		?>
 		
 			
 		<script type="text/javascript">
-			function wpedon_button_InsertShortcode() {
+			function gsml_button_InsertShortcode() {
 			
-				var id = document.getElementById("wpedon_button_id").value;
-				var wpedon_alignmentc = document.getElementById("wpedon_align");
-				var wpedon_alignmentb = wpedon_alignmentc.options[wpedon_alignmentc.selectedIndex].value;
+				var id = document.getElementById("gsml_button_id").value;
+				var gsml_alignmentc = document.getElementById("gsml_align");
+				var gsml_alignmentb = gsml_alignmentc.options[gsml_alignmentc.selectedIndex].value;
 				
 				if(id == "No buttons found.") { alert("Error: Please select an existing button from the dropdown or make a new one."); return false; }
 				if(id == "") { alert("Error: Please select an existing button from the dropdown or make a new one."); return false; }
 				
-				if(wpedon_alignmentb == "none") { var wpedon_alignment = ""; } else { var wpedon_alignment = ' align="' + wpedon_alignmentb + '"'; };
+				if(gsml_alignmentb == "none") { var gsml_alignment = ""; } else { var gsml_alignment = ' align="' + gsml_alignmentb + '"'; };
 				
-				window.send_to_editor('[wpedon id="' + id + '"' + wpedon_alignment + ']');
+				window.send_to_editor('[gsml id="' + id + '"' + gsml_alignment + ']');
 				
-				document.getElementById("wpedon_button_id").value = "";
-				wpedon_alignmentc.selectedIndex = null;
+				document.getElementById("gsml_button_id").value = "";
+				gsml_alignmentc.selectedIndex = null;
 			}
 		</script>
 
 		
-		<div id="wpedon_popup_container" style="display:none;">
+		<div id="gsml_popup_container" style="display:none;">
 		
 		
 			<h2>Insert a PayPal Donation Button</h2>
@@ -57,9 +58,9 @@ function wpedon_button_media_buttons_init() {
 			<table><tr><td>
 			
 			Choose an existing button: </td></tr><tr><td>
-			<select id="wpedon_button_id" name="wpedon_button_id">
+			<select id="gsml_button_id" name="gsml_button_id">
 				<?php
-				$args = array('post_type' => 'gandhipeacedonation_don_button','posts_per_page' => -1);
+				$args = array('post_type' => 'wpplugin_don_button','posts_per_page' => -1);
 
 				$posts = get_posts($args);
 
@@ -71,17 +72,14 @@ function wpedon_button_media_buttons_init() {
 
 						$id = $posts[$count]->ID;
 						$post_title = $posts[$count]->post_title;
-						$price = get_post_meta($id,'wpedon_button_price',true);
-						$sku = get_post_meta($id,'wpedon_button_id',true);
+						$price = get_post_meta($id,'gsml_button_price',true);
+						$sku = get_post_meta($id,'gsml_button_id',true);
 
 						echo "<option value='$id'>";
 						echo "Name: ";
 						echo $post_title;
 						echo " - Amount: ";
 						echo $price;
-						echo " - ID: ";
-						echo $sku;
-						echo "</option>";
 
 						$count++;
 					}
@@ -93,15 +91,15 @@ function wpedon_button_media_buttons_init() {
 				?>
 			</select>
 			</td></tr><tr><td>
-			Make a new button: <a target="_blank" href="admin.php?page=wpedon_buttons&action=new">here</a><br />
-			Manage existing buttons: <a target="_blank" href="admin.php?page=wpedon_buttons">here</a>
+			New button: <a target="_blank" href="admin.php?page=gsml_buttons&action=new">here</a><br />
+			Manage existing buttons: <a target="_blank" href="admin.php?page=gsml_buttons">here</a>
 			
 			</td></tr><tr><td>
 			<br />
 			</td></tr><tr><td>
 			
 			Alignment: </td></tr><tr><td>
-			<select id="wpedon_align" name="wpedon_align" style="width:100%;max-width:190px;">
+			<select id="gsml_align" name="gsml_align" style="width:100%;max-width:190px;">
 			<option value="none"></option>
 			<option value="left">Left</option>
 			<option value="center">Center</option>
@@ -112,7 +110,7 @@ function wpedon_button_media_buttons_init() {
 			<br />
 			</td></tr><tr><td>
 			
-			<input type="button" id="wpedon-paypal-insert" class="button-primary" onclick="wpedon_button_InsertShortcode();" value="Insert Button">		
+			<input type="button" id="gsml-paypal-insert" class="button-primary" onclick="gsml_button_InsertShortcode();" value="Insert Button">		
 			
 			</td></tr></table>
 		</div>
