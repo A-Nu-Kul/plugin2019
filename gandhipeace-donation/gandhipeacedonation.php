a409f008ddb6671758838440c480b506102c643d
Anukul Sharma <anukul.sharma@my.jcu.edu.au>
2019-06-07 12:02:55 +1000

    Update gandhipeacedonation.php
    This is the main plugin page

--- gandhipeace-donation/gandhipeacedonation.php
+++ gandhipeace-donation/gandhipeacedonation.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Gandhipeace Donation
 Plugin URI: http://anukuls.sgedu.site/plugin
-Description: Gandhipeace Donation is the donation plugin that gives user everything that need to start accepting donations today. Designed to integrate seamlessly with WordPress, Gandhipeacedonation allows the organization to create powerful fundraising platforms on their own website.Easy and simple setup and insertion of PayPal donate buttons with a shortcode or through a sidebar Widget. Donation purpose can be set for each button. A few other customization options are available as well.
+Description: Gandhipeace Donation is the donation plugin that gives user everything that need to start accepting donations for GSML.
 Author: Anukul Sharma
 Author URI: http://anukuls.sgedu.site/
 Version: 1.0.0
@@ -16,43 +16,37 @@ Text Domain: gandhipeace-donations
 */
 
 //// variables
-// plugin function 	  = wpedon
-// shortcode 		  = wpedon
+// plugin function 	  = gsml
+// shortcode 		  = gsml
 $plugin_basename = plugin_basename(__FILE__);
 
 
 //// plugin functions
-wpedon_gandhipeacedonation::init_wpedongandhipeacedonation();
+gsml_gandhipeacedonation::init_gsmlgandhipeacedonation();
 
-class wpedon_gandhipeacedonation {
-	public static function init_wpedongandhipeacedonation() {
-	register_deactivation_hook( __FILE__, array( __CLASS__, "wpedon_deactivate" ));
-	register_uninstall_hook( __FILE__, array( __CLASS__, "wpedon_uninstall" ));
+class gsml_gandhipeacedonation {
+	public static function init_gsmlgandhipeacedonation() {
+	register_deactivation_hook( __FILE__, array( __CLASS__, "gsml_deactivate" ));
+	register_uninstall_hook( __FILE__, array( __CLASS__, "gsml_uninstall" ));
 
-	$wpedon_settingsoptions = array(
-		'currency'    		=> '25',
-		'language'    		=> '3',
+	$gsml_settingsoptions = array(
 		'liveaccount'    	=> '',
-		'sandboxaccount'    => '',
-		'mode'    			=> '2',
+		'mode'    			=> '1',
 		'size'    			=> '2',
 		'opens'   			=> '2',
-		'no_note'    		=> '1',
-		'no_shipping'    	=> '1',
-		'cancel'    		=> '',
 		'return'    		=> '',
 		'note'    			=> '1',
 		'upload_image'    	=> '',
 	);
 
-	add_option("wpedon_settingsoptions", $wpedon_settingsoptions);
+	add_option("gsml_settingsoptions", $gsml_settingsoptions);
 	}
 	
-	function wpedon_deactivate() {
-		delete_option("wpedon_notice_shown");
+	function gsml_deactivate() {
+		delete_option("gsml_notice_shown");
 	}
 
-	function wpedon_uninstall() {
+	function gsml_uninstall() {
 	}
 }
 
@@ -78,9 +72,6 @@ include_once ('includes/public_shortcode.php');
 
 // private filters
 include_once ('includes/private_filters.php');
-
-// private widget
-include_once ('includes/private_widget.php');
 
 // public ipn
 include_once ('includes/public_ipn.php');
